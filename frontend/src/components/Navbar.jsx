1edb4f883bf4548f5fa1380a6433d5b498d50992
Iaak12 <ayazzkh1226@gmail.com>
2026-04-27 04:16:42 +0530

    style: switch to transparent svg fox logo to resolve background issues

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,10 @@ export default function Navbar() {
   return (
     <nav className="public-navbar">
       <Link to="/" className="public-logo" onClick={() => setIsOpen(false)}>
-        <img src="/fox_logo.png" alt="RankFox Logo" style={{ height: 42, width: 'auto', mixBlendMode: 'lighten' }} />
+        <svg width="42" height="42" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ flexShrink: 0 }}>
+          <path d="M12 2L3 9l2 13 7-3 7 3 2-13-9-7z" />
+          <path d="M9 11l3 3 3-3" />
+        </svg>
         <span>RankFox</span>
       </Link>
       
