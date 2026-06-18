6130c9af0663cbab2f1e02ba3f030233184a2efd
Iaak12 <ayazzkh1226@gmail.com>
2026-06-18 13:34:28 +0530

    Fix backend startup error: node-fetch import and add start script

--- backend/utils/realCrawler.js
+++ backend/utils/realCrawler.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 // Timeout helper
 function fetchWithTimeout(url, ms = 10000) {
